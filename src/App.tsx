df45f6e27a37761df7fe308347538638dea74d65
Carlos_Torres <carlos.a.torres.mtz@gmail.com>
2024-09-19 13:04:18 -0600

    Botton Exersice

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+import "./App.css";
+
+interface MyButtonProps {
+  count: number;
+  onClick: () => void;
+}
+
+function MyButton({ count, onClick }: MyButtonProps): JSX.Element {
+  return <button onClick={onClick}>Clicked {count} times</button>;
+}
+
+function App(): JSX.Element {
+  const [count, setCount] = useState<number>(0);
+  function handleClick(): void {
+    setCount(count + 1);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Counters that update together</h1>
+      <MyButton count={count} onClick={handleClick} />
+      <MyButton count={count} onClick={handleClick} />
     </div>
   );
 }
